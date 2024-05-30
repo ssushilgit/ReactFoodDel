6bd9c303710920042709d6828da45e2be441f481
Sushil Shrestha <165645824+ssushilgit@users.noreply.github.com>
2024-05-30 21:25:41 +0545

    Navbar, Header, ExploreMenu Components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ad, enim cumque quisquam hic dolorem harum sed maiores voluptatibus dicta dignissimos libero, ipsa id qui, odio nam. Exercitationem, accusamus odit.</p>
+            <button>View Menu</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
